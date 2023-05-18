d0a7cb2339035f6a9f8c2b7c15321ffc22245096
dckap <gokulakrishnanddckap@gmail.com>
2023-05-18 17:09:29 +0530

    solved the problem

--- index.php
+++ index.php
@@ -2,19 +2,50 @@
 // timestamp: 3:00
 //$array = [5, 2, 1];
 //$array = [1, 2, 5];
-$array = [4, 1, 2, 5];
+// $array = [4, 1, 2, 5];
+// $result = [];
+// if (isDescendingOrder($array)) {
+//     echo "The array is already arranged in descending order.";
+// } else {
+//     do {
+//         for ($i = 0; $i < count($array) - 1; $i++) {
+//             if ($array[$i] < $array[$i + 1]) {
+//                 $temp = $array[$i + 1];
+//                 $array[$i + 1] = $array[$i];
+//                 $array[$i] = $temp;
+//                 // $result[] = $array;
+//                 //
+//                 array_push($result, $array);
+//             }
+//         }
+//     } while (! isDescendingOrder($array));
+//     print_r($result);
+// }
+// function isDescendingOrder($array)
+// {
+//     for ($i = 0; $i < (count($array) - 1); $i++) {
+//         if ($array[$i] < $array[$i + 1]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+
+
+$array = [5,6,2,1];
 $result = [];
 if (isDescendingOrder($array)) {
     echo "The array is already arranged in descending order.";
 } else {
     do {
-        for ($i = 0; $i < count($array) - 1; $i++) {
-            if ($array[$i] < $array[$i + 1]) {
-                $temp = $array[$i + 1];
-                $array[$i + 1] = $array[$i];
+        for ($i = (count($array) -1) ; $i > 0; $i--) {
+            if ($array[$i] > $array[$i - 1]) {
+                $temp = $array[$i - 1];
+                $array[$i - 1] = $array[$i];
                 $array[$i] = $temp;
-                // $result[] = $array;
-                //
+
                 array_push($result, $array);
             }
         }
@@ -23,9 +54,11 @@ if (isDescendingOrder($array)) {
 }
 function isDescendingOrder($array)
 {
-    for ($i = 0; $i < (count($array) - 1); $i++) {
-        if ($array[$i] < $array[$i + 1]) {
-            return false;
+
+    for ($i = 0; $i < count($array) - 1; $i++) {
+
+        if ($array[$i+1] > $array[$i]) {
+             return false;
         }
     }
     return true;
